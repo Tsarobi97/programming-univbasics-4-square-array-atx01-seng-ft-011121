722e414a203ef5448680a46815b2776dabc8f734
Robert Obi <robobi97@googlemail.com>
2020-12-28 22:43:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -8,14 +8,3 @@ def square_array(array)
   end
 end
 
-def square_array(array)
-  array = [1,2,3]
-  square_array = []
-  
-  array.length.times do |index|
-    (array.to_i ** array.to_i)
-    
-      
-      
-
-    array[count].to_s
