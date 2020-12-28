5e0b825dbf00573d801e035581e3c80ee4196216
Robert Obi <robobi97@googlemail.com>
2020-12-28 22:56:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,13 +1,3 @@
-def square_array(array)
-  array = [1,2,3]
-  square_array = []
-  
-  for n in array do 
-    square_array << n ** 2
-  end 
-end
-  
-  
   def square_array(array)
     square_array = []
     counter = 0 
