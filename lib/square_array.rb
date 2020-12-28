8cde001b32e47670ce3dfc3894f414aa552ed018
Robert Obi <robobi97@googlemail.com>
2020-12-28 22:55:57 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -7,3 +7,13 @@ def square_array(array)
   end 
 end
   
+  
+  def square_array(array)
+    square_array = []
+    counter = 0 
+    while counter < array.length()
+    square_array.push(numbers[counter] * numbers[counter])
+    counter += 1
+  end
+  return square_array
+end
