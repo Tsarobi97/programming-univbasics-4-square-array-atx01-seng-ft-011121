32575384001444c97e839a8db1af8850124d6c0d
Robert Obi <robobi97@googlemail.com>
2020-12-28 22:05:39 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   array = [1,2,3]
   square_array = [1,4,9]
-  integer += 1
+  integer += *integer
   
   array.length.times do |index|
     square_array.push(array[index]*integer)
