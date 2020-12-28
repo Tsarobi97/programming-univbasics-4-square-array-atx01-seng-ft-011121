ac54a7c83dcb179adc9feee2d9a7e9fce8600fde
Robert Obi <robobi97@googlemail.com>
2020-12-28 22:57:10 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
-def square_array(array)
-  # your code here
+  def square_array(array)
+    square_array = []
+    counter = 0 
+    while counter < array.length()
+    square_array.push(array[counter] * array[counter] )
+    counter += 1
+  end
+  return square_array
 end
